e0ac0aa3c5bcab55e666e38d972e078c896b4035
bj5050 <43403792+bj5050@users.noreply.github.com>
2020-05-03 10:20:44 +0530

    excited
    wow

---
+++ projectlogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Form Design</title>
+		<link rel="stylesheet" type="text/css" href="">
+	</head>
+<body>
+	<div style="background-image: url('blood-1968457_960_720.png');">
+		 
+	<center>
+	<div class="container">
+    <h1>DONOR REGISTRATION</h1>		
+	<form method="POST" action="projectloginlogic.php">
+<div class="form_input">
+	<input type="text" name="uname" placeholder="Enter Your username"/>
+</div>
+<div class="form_input">
+	<input type="password" name="p" placeholder="Enter your password"/>
+</div>
+<input type="submit" name="submit" value="LOGIN" class="btn-login"/>
+</form>
+</div>
+</center>
+</body>
+</html>
